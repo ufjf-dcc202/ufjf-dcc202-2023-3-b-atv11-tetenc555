505530639a92b39fcc7896107da2cb4bd8d90bee
Estêvão Barbosa Fiorilo da Rocha <estevao.fiorilo@estudante.ufjf.br>
2023-11-21 22:17:02 -0300

    define ids e cria primeiro li pelo main

---
+++ main.js
@@ -0,0 +1,8 @@
+const pEntrada = document.querySelector("#entrada")
+const btnAdicionar = document.querySelector("#adicionar")
+const btnLimpar = document.querySelector("#limpar")
+const olItems = document.querySelector("#itens")
+
+const li = document.createElement("li")
+li.textContent= "Primeiro";
+olItems.appendChild(li);
